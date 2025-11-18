efb2c6b865ac21cf3b243f60a0235bff0c76077f
vivir2025 <fundacionnacerparavivir@gmail.com>
2025-11-18 09:55:39 -0500

    use

--- app/Http/Controllers/Api/CitaController.php
+++ app/Http/Controllers/Api/CitaController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Cita;
+use App\Models\Paciente;
 use App\Http\Resources\CitaResource;
 use Illuminate\Support\Facades\Log;
 
